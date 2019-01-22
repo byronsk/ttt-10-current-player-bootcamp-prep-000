87ea12bd6addab7bef63bc15cf4fefa0e2602904
vyskoutaris <vyskoutaris@hotmail.com>
2019-01-22 12:02:49 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
 
 def turn_count(board)
   counter = 0;
-  board.each do |member|
-    if member == "X" || member == "O"
+  board.each do |token|
+    if token == "X" || token == "O"
             counter += 1;
     end
   end
