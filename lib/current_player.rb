24cc84d90eefa707d7a16fca3478c5204a8cfd64
vyskoutaris <vyskoutaris@hotmail.com>
2019-01-22 11:54:24 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+def turn_count(board)
+  count = 0
+  board.do |token| 
+    if token == "X" | token == "O"
+      counter += 1
+  end
+ end
+ counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+ end
+
